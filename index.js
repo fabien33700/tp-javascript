8718e0ee3690adac2eee3a00dc0a326c5dd62f4f
Fabien LH <fabien.lehouedec@gmail.com>
2019-09-20 14:31:00 +0200

    Finished question 2

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 'use strict';
 
+let name = "Knight";
+let attack = 4;
+let defense = 2;
+let hp = 80;
+
+function displayMyPlayerInfo() {
+    console.log(`My name is ${name}, I have ${attack} attack, ${defense} defense and ${hp} health points.`)
+}
+
+displayMyPlayerInfo();

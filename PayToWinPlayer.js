77533b89c271ff5ac7b92b2d2ef1d6ee6f5a1c79
Fabien LH <fabien.lehouedec@gmail.com>
2019-09-22 22:18:33 +0200

    Removed non ES6 code in comments

--- PayToWinPlayer.js
+++ PayToWinPlayer.js
@@ -1,20 +1,6 @@
 const Player = require('./Player')
-/*
-function PayToWinPlayer(name, attack, defense) {
-    this.name = name;
-    this.attack = attack * 1.4;
-    this.defense = defense;
-    this.hp = Player.prototype.DEFAULT_HP;
-}
-
-PayToWinPlayer.prototype = Object.create(Player.prototype);
-
-module.exports = PayToWinPlayer;
-
-*/
 
 module.exports = class PayToWinPlayer extends Player {
-
     constructor (name, attack, defense) {
         super(name, attack * 1.4, defense);
     }

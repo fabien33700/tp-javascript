e75b4d852dcae02f34d3aa273d4a65be3d4f33c1
Fabien LH <fabien.lehouedec@gmail.com>
2019-10-04 02:27:06 +0200

    Replaced chain of promises by an async recursive function

--- Player.js
+++ Player.js
@@ -3,41 +3,37 @@
 const rp = require('request-promise')
 const baseUrl = "https://www.random.org/integers/"
 
-const DEBUG = false
 const DEFAULT_HP = 10
 
 function getMyPlayerRatio(player, number) {
     return player.attack - number
 }
 
-function randomNumber(min, max) {
+async function randomNumber(max) {
     return rp({
         url: baseUrl,
         qs: {
             num: 1,
-            min: min,
+            min: 1,
             max: max,
             col: 1,
             base: 10,
             format: "plain"
         }
     })
-    .then(output => parseInt(output.replace("\n", "")))
-    .catch(err => console.error(err))
+        .then(output => parseInt(output.replace("\n", "")))
+        .catch(err => console.error(err))
 }
 
-function randomSequence() {
-    const ps = [Promise.resolve(100)]
-    for (let i = 0; i < 10; i++) {
-        const p = ps[ps.length-1]
-            .then(n => randomNumber(1, Math.ceil(n * 1.2)))
-        ps.push(p)
-    }
-    return Promise.all(ps).catch(console.error)
+async function randomSequence(max = 100, c = 10) {
+    if (c === 0) return [max]
+
+    return [max, ... await randomSequence(
+        await randomNumber(Math.ceil(1.2 * max)), c - 1)]
 }
 
 function hofCleanValues(fn) {
-    return (...params) => 
+    return (...params) =>
         fn(...params).then(nums => nums
             .filter(n => n >= 10)
             .map((n, i) => n * (n % 10 === i ? 2 : 1))
@@ -47,7 +43,7 @@ function hofCleanValues(fn) {
 }
 
 module.exports = class Player {
-    constructor (name, attack, defense) {
+    constructor(name, attack, defense) {
         this.name = name
         this.attack = attack
         this.defense = defense
@@ -64,13 +60,13 @@ module.exports = class Player {
         other.ratio = ratio2
         let winner = (ratio1 >= ratio2) ? this : other
         let looser = (ratio1 >= ratio2) ? other : this
-    
+
         looser.hp = 0
         winner.hp = looser.ratio * (winner.hp / winner.ratio)
         console.log('Winner is : \n')
         winner.displayMyPlayerInfo()
     }
-    displayMyPlayerInfo () {
+    displayMyPlayerInfo() {
         console.log(`My name is ${this.name}, I have ${this.attack} attack, ${this.defense} defense and ${this.hp} health points.`)
     }
 }
